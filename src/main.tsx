378debe1ba698bddd8f982980b061192ae16b4da
thealmighty3096 <phanikumarc24@iimklive.com>
2025-01-25 19:08:12 +0530

    vercel analyticschanges

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
